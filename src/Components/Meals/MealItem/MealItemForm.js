2cf93d51e22008219d4e8b35852643d3ca52abd9
SushmaGowda7 <110340352+SushmaGowda7@users.noreply.github.com>
2022-09-26 15:02:40 +0530

    added a form for user to select number of quantity of item

--- src/Components/Meals/MealItem/MealItemForm.js
+++ src/Components/Meals/MealItem/MealItemForm.js
@@ -1,7 +1,24 @@
+import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
+    return(
+        <form className={classes.form}>
+            <Input
+                label='Amount'
+                input={{
+                    id:'amount'+ props.id,
+                    type: 'number',
+                    min: '1',
+                    max: '9',
+                    step: '1',
+                    defaultValue: '1'
+                }}
 
+            />
+            <button className={classes.button}>+ Add</button>
+        </form>
+    );
 };
 
 export default MealItemForm;
--- src/Components/Meals/MealItemsAvailable.js
+++ src/Components/Meals/MealItemsAvailable.js
@@ -32,6 +32,7 @@ const DummyMeals = [
 const MealItemsAvailable = () => {
     const mealsList = DummyMeals.map((meal) => (
       <MealItem
+      id={meal.id}
        key={meal.id} 
        name={meal.name} 
        description={meal.description} 
